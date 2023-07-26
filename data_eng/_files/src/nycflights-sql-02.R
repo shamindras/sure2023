534d459d1bb42541cbf1587c1a4518715efb0325
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2023-07-26 16:33:42 -0400

    feat: update data engineering lecture 6 pdf

--- data_eng/_files/src/nycflights-sql-02.R
+++ data_eng/_files/src/nycflights-sql-02.R
@@ -1,6 +1,6 @@
 # Install the RSQLite packages and the NYC flights data ----
-REQ_PKGS <- c("dittodb", "RSQLite", "nycflights13", "tidyquery", "dbplyr")
-# install.packages(REQ_PKGS)
+REQ_PKGS <- c("dittodb", "RSQLite", "nycflights13")
+install.packages(REQ_PKGS)
 # pak::pkg_install(REQ_PKGS)
 
 # Load the required packages ----
@@ -8,8 +8,6 @@ library(RSQLite)
 library(nycflights13)
 library(dittodb)
 library(tidyverse)
-library(tidyquery)
-library(dbplyr)
 
 # Set up the connection to the NYC flights database ----
 NYC_CONN <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
@@ -26,108 +24,84 @@ fetch_query <- function(query, con = NYC_CONN) {
 
 # We can now run some custom queries on the NYC flights data ----
 # fetch_query("SELECT * FROM flights;")
-fetch_query("SELECT * FROM flights;")
 fetch_query("SELECT * FROM flights LIMIT 10;")
 fetch_query("SELECT * FROM weather LIMIT 10;")
 fetch_query("SELECT * FROM planes LIMIT 10;")
 fetch_query("SELECT * FROM airports LIMIT 10;")
 fetch_query("SELECT * FROM airlines LIMIT 10;")
 
+# Fetch specific columns from flights
+fetch_query("SELECT dep_time, arr_time, flight FROM flights LIMIT 10;")
+flights %>%
+  select(dep_time, arr_time, flight) %>%
+  head(10)
+
+# Let's get speed of flights
+fetch_query("SELECT flight, distance/(air_time/60) AS speed FROM flights LIMIT 10;")
+flights %>%
+  mutate(speed = distance / (air_time / 60)) %>%
+  select(flight, speed) %>%
+  head(10)
+
+fetch_query("SELECT MIN(air_time) AS min_ar, MAX(air_time) AS max_ar from flights;")
+fetch_query("SELECT COUNT(*) from flights;")
+
+fetch_query('SELECT * FROM flights WHERE origin = "JFK" LIMIT 10;')
+
+fetch_query('SELECT COUNT(*) FROM flights WHERE dest != "JFK";')
+
+fetch_query('SELECT * FROM flights WHERE tailnum IN ("N593JB", "N532UA") LIMIT 20;')
+
+fetch_query("SELECT * FROM flights WHERE month NOT IN (1, 12) AND arr_delay > 120 LIMIT 10;")
+flights %>%
+  filter(!(month %in% c(1, 12)) & arr_delay > 120) %>%
+  head(20)
+
+fetch_query("SELECT * FROM weather WHERE wind_gust IS NOT NULL;")
+weather %>%
+  filter(!is.na(wind_gust)) %>%
+  head(20)
+weather %>%
+  drop_na(wind_gust) %>%
+  head(20)
+
+fetch_query("SELECT origin, AVG(arr_delay) AS avd FROM flights GROUP BY origin;")
+flights %>%
+  group_by(origin) %>%
+  summarize(vcd = mean(arr_delay, na.rm = TRUE))
+
 fetch_query(
-  'SELECT dest, month, day,
+  "SELECT dest, month, day,
 MIN(arr_delay) AS mnd,
 MAX(arr_delay) AS mxd,
 AVG(arr_delay) AS avd
 FROM flights
 GROUP BY dest, month, day
-LIMIT 10;')
+LIMIT 10;"
+)
 
-tidyquery::query('SELECT dest, month, day,
-MIN(arr_delay) AS mnd,
-MAX(arr_delay) AS mxd,
-AVG(arr_delay) AS avd
-FROM nycflights13::flights
-GROUP BY dest, month, day
-LIMIT 10;')
+fetch_query("SELECT flight, distance/(air_time/60) AS speed FROM flights ORDER BY distance/(air_time/60) DESC LIMIT 10;")
 
-tidyquery::show_dplyr('SELECT dest, month, day,
-MIN(arr_delay) AS mnd,
-MAX(arr_delay) AS mxd,
-AVG(arr_delay) AS avd
-FROM nycflights13::flights
-GROUP BY dest, month, day
-LIMIT 10;')
+fetch_query("SELECT engines, COUNT(*) AS tot_num FROM planes GROUP BY engines HAVING tot_num < 200;")
+planes %>%
+  count(engines, name = "tot_num") %>%
+  filter(tot_num < 200)
 
-# This is the code returned from
-flight_summ <-
-  tbl(NYC_CONN, "flights") %>%
-  group_by(dest, month, day) %>%
-  summarise(mnd = min(arr_delay, na.rm = TRUE),
-            mxd = max(arr_delay, na.rm = TRUE),
-            avd = mean(arr_delay, na.rm = TRUE)) %>%
-  ungroup() %>% head(10)
-
-flight_summ %>% dbplyr::sql_render() %>% cat()
-
-
-nycflights13::flights %>%
-  filter(dest == "ALB", month == 12, day == 10)
-
-fetch_query('SELECT DISTINCT * FROM flights;')
-fetch_query('SELECT DISTINCT * FROM flights LIMIT 10;')
-fetch_query('SELECT DISTINCT carrier, flight FROM flights ORDER BY carrier, flight;')
-
-fetch_query('SELECT year, month, day, arr_delay,
-            CASE WHEN arr_delay < 0 THEN "Early"
-                 WHEN arr_delay = 0 THEN "On Time"
-                 WHEN arr_delay > 0 THEN "Late"
-                 ELSE "Unknown"
-            END AS delay_type
-            FROM flights
-            LIMIT 10;')
-
-fetch_query('SELECT * FROM planes LIMIT 10;')
-fetch_query('SELECT manufacturer, COUNT(DISTINCT type) AS tot_uniq_types from planes GROUP BY manufacturer ORDER BY manufacturer;')
-fetch_query('SELECT manufacturer, COUNT(*) AS tot_planes from planes GROUP BY manufacturer ORDER BY manufacturer;')
-
-
-fetch_query('SELECT manufacturer, COUNT(*) AS tot_planes from planes GROUP BY manufacturer ORDER BY manufacturer;')
-
-fetch_query('SELECT DISTINCT dest FROM flights ORDER BY dest;')
-fetch_query('SELECT DISTINCT dest FROM flights WHERE dest LIKE "M%";')
-fetch_query('SELECT DISTINCT dest FROM flights WHERE dest LIKE "M%" ORDER BY dest;')
-
-fetch_query('SELECT dest, COUNT(*) as tot_flights
-FROM flights
-WHERE dest IN ("MIA", "MCO", "MSP", "MSY", "MKE", "MEM", "MYR", "MDW", "MHT", "MSN", "MCI", "MTJ", "MVY")
-GROUP BY dest')
-
-fetch_query('SELECT dest, COUNT(*) as tot_flights
-FROM flights
-WHERE dest IN (SELECT DISTINCT dest FROM flights WHERE dest LIKE "M%")
-GROUP BY dest')
-
-fetch_query('SELECT * FROM flights LIMIT 10;')
-fetch_query('SELECT * FROM airlines;')
-
+# Or we can do this the more manual way
+planes %>%
+  group_by(engines) %>%
+  summarize(tot_num = n()) %>%
+  filter(tot_num < 200)
 
 fetch_query(
-  'SELECT carrier, COUNT(*) as tot_flights
-   FROM flights
-   WHERE month IN (6, 7)
-   GROUP BY carrier
-   ORDER BY carrier;') %>%
-  view()
-
-
-fetch_query(
-  'SELECT al.name AS airline, COUNT(*) as tot_flights
-  FROM flights AS fl
-  LEFT JOIN airlines AS al
-    ON al.carrier = fl.carrier
-  WHERE month IN (6, 7)
-  GROUP BY al.name;') %>%
-  view()
+  "SELECT dest, month, day,
+MIN(arr_delay) AS mnd,  MAX(arr_delay) AS mxd,
+AVG(arr_delay) AS avd
+FROM flights
+GROUP BY dest, month, day
+ORDER BY dest, month DESC, day
+LIMIT 10;"
+)
 
 # Close the connection to the database ----
 DBI::dbDisconnect(NYC_CONN)
